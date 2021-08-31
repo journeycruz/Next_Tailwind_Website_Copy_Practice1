8ccf6b63bd8e9e001768f900c3263679ec977cd8
Journey Cruz <journey.cruz@gmail.com>
2021-08-31 13:32:20 -0500

    Initialize Next app with Tailwind

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Hulu 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Let's build Hulu 2.0</h1>
+    </div>
+  )
+}
